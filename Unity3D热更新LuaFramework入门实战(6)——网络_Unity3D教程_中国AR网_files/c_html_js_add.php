56e536ad23ed4efbc1d7811388e59dbf0bc220cd
xiaominlai <419875981@qq.com>
2017-05-09 13:48:22 +0800

    保存内容

---
+++ Unity3D热更新LuaFramework入门实战(6)——网络_Unity3D教程_中国AR网_files/c_html_js_add.php
@@ -0,0 +1,12 @@
+var bloghost = "http://www.chinaar.com/";
+var cookiespath = "/";
+var ajaxurl = "http://www.chinaar.com/zb_system/cmd.php?act=ajax&src=";
+var lang_comment_name_error = "名称不能为空或格式不正确";
+var lang_comment_email_error = "邮箱格式不正确，可能过长或为空";
+var lang_comment_content_error = "评论内容不能为空或过长";
+
+$(function () {var $cpLogin = $(".cp-login").find("a");var $cpVrs = $(".cp-vrs").find("a");SetCookie("timezone", (new Date().getTimezoneOffset()/60)*(-1));var $addoninfo = GetCookie("addinfo");if(!$addoninfo){LoadRememberInfo();return ;}$addoninfo = eval("("+$addoninfo+")");if($addoninfo.chkadmin){$(".cp-hello").html("欢迎 " + $addoninfo.useralias + " (" + $addoninfo.levelname  + ")");if ($cpLogin.length == 1 && $cpLogin.html().indexOf("[") > -1) { $cpLogin.html("[后台管理]");} else {$cpLogin.html("后台管理");};}if($addoninfo.chkarticle){if ($cpLogin.length == 1 && $cpVrs.html().indexOf("[") > -1) {$cpVrs.html("[新建文章]"); } else {$cpVrs.html("新建文章");};$cpVrs.attr("href", bloghost + "zb_system/cmd.php?act=ArticleEdt");}});
+
+var $zbpzanalert = "您已经点过赞了"
+
+document.writeln("<script src='http://www.chinaar.com/zb_users/plugin/UEditor/third-party/prism/prism.js' type='text/javascript'></script><link rel='stylesheet' type='text/css' href='http://www.chinaar.com/zb_users/plugin/UEditor/third-party/prism/prism.css'/>");$(function(){var compatibility={as3:"actionscript","c#":"csharp",delphi:"pascal",html:"markup",xml:"markup",vb:"basic",js:"javascript",plain:"markdown",pl:"perl",ps:"powershell"};if(document.querySelectorAll){var runFunction=function(doms,callback){for(var i=0;i<doms.length;i++){var preDom=doms.item(i);var codeDom=document.createElement("code");if(callback)callback(preDom);codeDom.innerHTML=preDom.innerHTML;codeDom.className="language-"+function(classObject){if(classObject===null)return"markdown";var className=classObject[1];return compatibility[className]?compatibility[className]:className}(preDom.className.match(/prism-language-([0-9a-zA-Z]+)/))+" prism-line-numbers";preDom.innerHTML="";preDom.appendChild(codeDom)}};runFunction(document.querySelectorAll("pre.prism-highlight"));runFunction(document.querySelectorAll('pre[class*="brush:"]'),function(preDom){var original;if((original=preDom.className.match(/brush:([a-zA-Z0-9\#]+);/))!==null){preDom.className="prism-highlight prism-language-"+original[1]}})}});
---
+++ Unity3D热更新LuaFramework入门实战(9)——线程管理器_Unity3D教程_中国AR网_files/main.js
@@ -0,0 +1,99 @@
+/*
+For: 仿知乎动态背景[ZBLOG-PHP插件]
+Author: 尔今
+Author Email: erx@qq.com
+Author URL: http://www.yiwuku.com/
+*/
+
+(function(){
+	document.writeln('<canvas id="AnimateBg" style="position:fixed;top:0;left:0;background:'+anbgColor+';z-index:-1;"></canvas>');
+	var WIDTH = window.innerWidth, HEIGHT = window.innerHeight, POINT = anbgPnum;
+	if(window.innerWidth<800){
+		WIDTH = 640;
+		HEIGHT = 800;
+		POINT = 18;
+	}
+	var canvas = document.getElementById('AnimateBg');
+	canvas.width = WIDTH,
+	canvas.height = HEIGHT;
+	var context = canvas.getContext('2d');
+	context.strokeStyle = 'rgba(0,0,0,0.2)',
+	context.strokeWidth = 1,
+	context.fillStyle = 'rgba(0,0,0,0.1)';
+	var circleArr = [];
+	function Line (x, y, _x, _y, o) {
+		this.beginX = x,
+		this.beginY = y,
+		this.closeX = _x,
+		this.closeY = _y,
+		this.o = o;
+	}
+	function Circle (x, y, r, moveX, moveY) {
+		this.x = x,
+		this.y = y,
+		this.r = r,
+		this.moveX = moveX,
+		this.moveY = moveY;
+	}
+	function num (max, _min) {
+		var min = arguments[1] || 0;
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+	function drawCricle (cxt, x, y, r, moveX, moveY) {
+		var circle = new Circle(x, y, r, moveX, moveY)
+		cxt.beginPath()
+		cxt.arc(circle.x, circle.y, circle.r, 0, 2*Math.PI)
+		cxt.closePath()
+		cxt.fill();
+		return circle;
+	}
+	function drawLine (cxt, x, y, _x, _y, o) {
+		var line = new Line(x, y, _x, _y, o)
+		cxt.beginPath()
+		cxt.strokeStyle = 'rgba(0,0,0,'+ o +')'
+		cxt.moveTo(line.beginX, line.beginY)
+		cxt.lineTo(line.closeX, line.closeY)
+		cxt.closePath()
+		cxt.stroke();
+	}
+	function anbg_init () {
+		circleArr = [];
+		for (var i = 0; i < POINT; i++) {
+			circleArr.push(drawCricle(context, num(WIDTH), num(HEIGHT), num(15, 2), num(10, -10)/40, num(10, -10)/40));
+		}
+		draw();
+	}
+	function draw () {
+		context.clearRect(0,0,canvas.width, canvas.height);
+		for (var i = 0; i < POINT; i++) {
+			drawCricle(context, circleArr[i].x, circleArr[i].y, circleArr[i].r);
+		}
+		for (var i = 0; i < POINT; i++) {
+			for (var j = 0; j < POINT; j++) {
+				if (i + j < POINT) {
+					var A = Math.abs(circleArr[i+j].x - circleArr[i].x),
+						B = Math.abs(circleArr[i+j].y - circleArr[i].y);
+					var lineLength = Math.sqrt(A*A + B*B);
+					var C = 1/lineLength*7-0.009;
+					var lineOpacity = C > 0.03 ? 0.03 : C;
+					if (lineOpacity > 0) {
+						drawLine(context, circleArr[i].x, circleArr[i].y, circleArr[i+j].x, circleArr[i+j].y, lineOpacity);
+					}
+				}
+			}
+		}
+	}
+	anbg_init();
+	setInterval(function () {
+		for (var i = 0; i < POINT; i++) {
+			var cir = circleArr[i];
+			cir.x += cir.moveX;
+			cir.y += cir.moveY;
+			if (cir.x > WIDTH) cir.x = 0;
+			else if (cir.x < 0) cir.x = WIDTH;
+			if (cir.y > HEIGHT) cir.y = 0;
+			else if (cir.y < 0) cir.y = HEIGHT;
+		}
+		draw();
+	}, anbgSpeed);
+})();
